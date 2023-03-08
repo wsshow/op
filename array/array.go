3456652a7679bed265ff26b8628bd67897619f5f
ws <wangshang@nj.iscas.ac.cn>
2023-03-08 14:30:17 +0800

    fix(array): [func: RemoveAll, error: index out of range]

--- array/array.go
+++ array/array.go
@@ -5,21 +5,20 @@ import (
 )
 
 type Array struct {
-	data []interface{}
+	data []any
 }
 
 func NewArray() *Array {
 	return new(Array)
 }
 
-func (a *Array) Add(elems ...interface{}) {
+func (a *Array) Add(elems ...any) {
 	a.data = append(a.data, elems...)
 }
 
-func (a *Array) Remove(e interface{}) {
+func (a *Array) Remove(e any) {
 	d := a.data
-	cnt := len(d)
-	for i := 0; i < cnt; i++ {
+	for i, cnt := 0, len(d); i < cnt; i++ {
 		if d[i] == e {
 			d = append(d[:i], d[i+1:]...)
 			break
@@ -28,23 +27,21 @@ func (a *Array) Remove(e interface{}) {
 	a.data = d
 }
 
-func (a *Array) RemoveAll(e interface{}) {
+func (a *Array) RemoveAll(e any) {
 	d := a.data
-	cnt := len(d)
-	for i := 0; i < cnt; i++ {
+	for i := 0; i < len(d); {
 		if d[i] == e {
 			d = append(d[:i], d[i+1:]...)
-			continue
+		} else {
+			i++
 		}
 	}
 	a.data = d
 }
 
-func (a *Array) Contain(e interface{}) bool {
-	d := a.data
-	cnt := len(d)
-	for i := 0; i < cnt; i++ {
-		if d[i] == e {
+func (a *Array) Contain(e any) bool {
+	for _, v := range a.data {
+		if v == e {
 			return true
 		}
 	}
@@ -55,7 +52,7 @@ func (a *Array) Count() int {
 	return len(a.data)
 }
 
-func (a *Array) ForEach(f func(interface{})) {
+func (a *Array) ForEach(f func(any)) {
 	for _, v := range a.data {
 		f(v)
 	}
@@ -65,7 +62,7 @@ func (a *Array) Clear() {
 	a.data = nil
 }
 
-func (a *Array) Data() []interface{} {
+func (a *Array) Data() []any {
 	return a.data
 }
 
@@ -73,7 +70,7 @@ func (a *Array) Sort(less func(i, j int) bool) {
 	sort.Slice(a.data, less)
 }
 
-func (a *Array) Filter(f func(interface{}) bool) *Array {
+func (a *Array) Filter(f func(any) bool) *Array {
 	na := NewArray()
 	for _, v := range a.data {
 		if f(v) {
@@ -83,7 +80,7 @@ func (a *Array) Filter(f func(interface{}) bool) *Array {
 	return na
 }
 
-func (a *Array) Map(f func(interface{}) interface{}) *Array {
+func (a *Array) Map(f func(any) any) *Array {
 	na := NewArray()
 	for _, v := range a.data {
 		na.Add(f(v))
