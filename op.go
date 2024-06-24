be80c3766ac987f2d11ad4354a4877d6915ff25c
ws <wsshow@foxmail.com>
2024-06-24 15:09:04 +0800

    style: comment

--- op.go
+++ op.go
@@ -8,46 +8,28 @@ import (
 	"github.com/wsshow/op/workerpool"
 )
 
-/**
- * @Description: 创建一个字符串对象
- * @param s
- * @return *String
- */
+// 创建一个字符串对象
 func NewString(s string) *str.String {
 	return str.NewString(s)
 }
 
-/**
- * @Description: 创建一个切片对象
- * @param values
- * @return *Slice
- */
+// 创建一个切片对象
 func NewSlice[T any](values ...T) *slice.Slice[T] {
 	return slice.New(values...)
 }
 
-/**
- * @Description: 创建一个双端队列对象
- * @param size 0: 最大容量 1: 最小容量(容量缩放时保证容量不小于最小容量)
- * @return *Deque
- */
+// 创建一个双端队列对象
+// size 0: 最大容量 1: 最小容量(容量缩放时保证容量不小于最小容量)
 func NewDeque[T any](size ...int) *deque.Deque[T] {
 	return deque.New[T](size...)
 }
 
-/**
- * @Description: 创建一个工作池对象
- * @param maxWorkers
- * @return *WorkerPool
- */
+// 创建一个工作池对象
 func NewWorkerPool(maxWorkers int) *workerpool.WorkerPool {
 	return workerpool.New(maxWorkers)
 }
 
-/**
- * @Description: 创建一个事件发射器对象
- * @return *Emitter
- */
+// 创建一个事件发射器对象
 func NewEmitter() *emission.Emitter {
 	return emission.NewEmitter()
 }
